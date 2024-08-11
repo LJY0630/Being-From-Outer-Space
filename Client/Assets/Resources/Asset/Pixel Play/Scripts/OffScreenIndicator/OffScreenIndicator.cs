bad76515572f709a43d4a2d6e74fb2ef7d3508e3
LJY0630 <118187321+LJY0630@users.noreply.github.com>
2024-08-11 21:25:22 +0900

    1.0.0

---
+++ Client/Assets/Resources/Asset/Pixel Play/Scripts/OffScreenIndicator/OffScreenIndicator.cs
@@ -0,0 +1,155 @@
+﻿using PixelPlay.OffScreenIndicator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Attach the script to the off screen indicator panel.
+/// </summary>
+[DefaultExecutionOrder(-1)]
+public class OffScreenIndicator : MonoBehaviour
+{
+    [Range(0.5f, 0.9f)]
+    [Tooltip("Distance offset of the indicators from the centre of the screen")]
+    [SerializeField] private float screenBoundOffset = 0.9f;
+
+    [SerializeField]
+    private Camera camera;
+
+    [SerializeField]
+    private Canvas canvas;
+
+    [SerializeField]
+    private GameObject Panel;
+    private RectTransform rect;
+    private Vector3 screenCentre;
+    private Vector3 screenBounds;
+    private Vector2 canvasPosition;
+    Vector2 screenPoint;
+
+    private List<Target> targets = new List<Target>();
+
+    public static Action<Target, bool> TargetStateChanged;
+
+    void Awake()
+    {
+        rect = Panel.GetComponent<RectTransform>();
+        screenCentre = new Vector3(rect.rect.width, rect.rect.height, 0) / 2;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, rect.position, canvas.worldCamera, out canvasPosition);
+        canvasPosition.x += canvas.GetComponent<RectTransform>().rect.width / 2;
+        canvasPosition.y += canvas.GetComponent<RectTransform>().rect.height / 2;
+        screenBounds = screenCentre * screenBoundOffset;
+        TargetStateChanged += HandleTargetStateChanged;
+    }
+
+    void Update()
+    {
+        DrawIndicators();
+    }
+
+    /// <summary>
+    /// Draw the indicators on the screen and set thier position and rotation and other properties.
+    /// </summary>
+    void DrawIndicators()
+    {
+        foreach(Target target in targets)
+        {
+            Vector3 screenPosition = OffScreenIndicatorCore.GetScreenPosition(camera, target.transform.position);
+            bool isTargetVisible = OffScreenIndicatorCore.IsTargetVisible(screenPosition, rect);
+            float distanceFromCamera = target.NeedDistanceText ? target.GetDistanceFromCamera(camera.transform.position) : float.MinValue;// Gets the target distance from the camera.
+            Indicator indicator = null;
+
+            if(target.NeedBoxIndicator && isTargetVisible)
+            {
+                screenPosition.z = 0;
+                indicator = GetIndicator(ref target.indicator, IndicatorType.BOX); // Gets the box indicator from the pool.
+            }
+            else if(target.NeedArrowIndicator && !isTargetVisible)
+            {
+                float angle = float.MinValue;
+                OffScreenIndicatorCore.GetArrowIndicatorPositionAndAngle(ref screenPosition, ref angle, screenCentre, screenBounds, rect, new Vector3(canvasPosition.x, canvasPosition.y, 0));
+                indicator = GetIndicator(ref target.indicator, IndicatorType.ARROW); // Gets the arrow indicator from the pool.
+               indicator.transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
+            }
+
+            if(indicator)
+            {
+                indicator.SetImageColor(target.TargetColor);// Sets the image color of the indicator.
+                indicator.SetDistanceText(distanceFromCamera); //Set the distance text for the indicator.
+                if (target.NeedArrowIndicator && !isTargetVisible)
+                {
+                    indicator.transform.position = screenPosition; //Sets the position of the indicator on the screen.
+                }
+                else 
+                {
+                    Vector3 scaledScreenPosition = new Vector3(screenPosition.x * rect.localScale.x, screenPosition.y * rect.localScale.y, 0);
+
+                    // 패널의 중심을 계산합니다.
+                    Vector3 panelCenter = new Vector3(rect.rect.width * rect.localScale.x / 2f, rect.rect.height * rect.localScale.y / 2f, 0);
+
+                    // indicator의 위치를 설정합니다.
+                    indicator.transform.position = scaledScreenPosition + new Vector3(canvasPosition.x, canvasPosition.y, 0) - panelCenter;
+                }
+                indicator.SetTextRotation(Quaternion.identity); // Sets the rotation of the distance text of the indicator.
+            }
+        }
+    }
+
+    /// <summary>
+    /// 1. Add the target to targets list if <paramref name="active"/> is true.
+    /// 2. If <paramref name="active"/> is false deactivate the targets indicator, 
+    ///     set its reference null and remove it from the targets list.
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="active"></param>
+    private void HandleTargetStateChanged(Target target, bool active)
+    {
+        if(active)
+        {
+            targets.Add(target);
+        }
+        else
+        {
+            target.indicator?.Activate(false);
+            target.indicator = null;
+            targets.Remove(target);
+        }
+    }
+
+    /// <summary>
+    /// Get the indicator for the target.
+    /// 1. If its not null and of the same required <paramref name="type"/> 
+    ///     then return the same indicator;
+    /// 2. If its not null but is of different type from <paramref name="type"/> 
+    ///     then deactivate the old reference so that it returns to the pool 
+    ///     and request one of another type from pool.
+    /// 3. If its null then request one from the pool of <paramref name="type"/>.
+    /// </summary>
+    /// <param name="indicator"></param>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    private Indicator GetIndicator(ref Indicator indicator, IndicatorType type)
+    {
+        if(indicator != null)
+        {
+            if(indicator.Type != type)
+            {
+                indicator.Activate(false);
+                indicator = type == IndicatorType.BOX ? BoxObjectPool.current.GetPooledObject() : ArrowObjectPool.current.GetPooledObject();
+                indicator.Activate(true); // Sets the indicator as active.
+            }
+        }
+        else
+        {
+            indicator = type == IndicatorType.BOX ? BoxObjectPool.current.GetPooledObject() : ArrowObjectPool.current.GetPooledObject();
+            indicator.Activate(true); // Sets the indicator as active.
+        }
+        return indicator;
+    }
+
+    private void OnDestroy()
+    {
+        TargetStateChanged -= HandleTargetStateChanged;
+    }
+}
---
+++ Client/Assets/Resources/Asset/Pixel Play/Scripts/OffScreenIndicator/OffScreenIndicatorCore.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace PixelPlay.OffScreenIndicator
+{
+    public class OffScreenIndicatorCore
+    {
+        /// <summary>
+        /// Gets the position of the target mapped to screen cordinates.
+        /// </summary>
+        /// <param name="mainCamera">Refrence to the main camera</param>
+        /// <param name="targetPosition">Target position</param>
+        /// <returns></returns>
+        public static Vector3 GetScreenPosition(Camera mainCamera, Vector3 targetPosition)
+        {
+            Vector3 screenPosition = mainCamera.WorldToScreenPoint(targetPosition);
+            return screenPosition;
+        }
+
+        /// <summary>
+        /// Gets if the target is within the view frustrum.
+        /// </summary>
+        /// <param name="screenPosition">Position of the target mapped to screen cordinates</param>
+        /// <returns></returns>
+        public static bool IsTargetVisible(Vector3 screenPosition, RectTransform rect)
+        {
+            bool isTargetVisible = screenPosition.z >= 0 && screenPosition.x >= 0 && screenPosition.x <= 256.1 && screenPosition.y >= 0 && screenPosition.y <= 256.1;
+            return isTargetVisible;
+        }
+
+        /// <summary>
+        /// Gets the screen position and angle for the arrow indicator. 
+        /// </summary>
+        /// <param name="screenPosition">Position of the target mapped to screen cordinates</param>
+        /// <param name="angle">Angle of the arrow</param>
+        /// <param name="screenCentre">The screen  centre</param>
+        /// <param name="screenBounds">The screen bounds</param>
+        public static void GetArrowIndicatorPositionAndAngle(ref Vector3 screenPosition, ref float angle, Vector3 screenCentre, Vector3 screenBounds, RectTransform panelRect, Vector3 where)
+        {
+            screenPosition -= screenCentre;
+
+            // When the targets are behind the camera their projections on the screen (WorldToScreenPoint) are inverted,
+            // so just invert them.
+            if (screenPosition.z < 0)
+            {
+                screenPosition *= -1;
+            }
+
+            screenPosition.x *= panelRect.localScale.x;
+            screenPosition.y *= panelRect.localScale.y;
+
+            // Angle between the x-axis (bottom of screen) and a vector starting at zero (bottom-left corner of screen) and terminating at screenPosition.
+            angle = Mathf.Atan2(screenPosition.y, screenPosition.x);
+
+            // Calculate the maximum x and y position within the panel's boundaries
+            float maxX = panelRect.rect.width * panelRect.localScale.x / 2f;
+            float maxY = panelRect.rect.height * panelRect.localScale.y / 2f;
+
+            // Adjust the x and y positions to fit within the panel's boundaries
+            screenPosition.x = Mathf.Clamp(screenPosition.x, -maxX, maxX);
+            screenPosition.y = Mathf.Clamp(screenPosition.y, -maxY, maxY);
+            screenPosition.z = 0.0f;
+
+            // Bring the ScreenPosition back to its original reference.
+            screenPosition += where;
+        }
+    }
+}
